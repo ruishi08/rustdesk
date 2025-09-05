f3c81987d12e0cc9ade9a5b2d92fb031c7a11438
ruishi08 <ruishi08@outlook.com>
2025-09-05 10:25:18 +0800

    修改替换 API 地址

--- src/common.rs
+++ src/common.rs
@@ -1024,7 +1024,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://150.230.105.194:21114".to_owned()
 }
 
 #[inline]
